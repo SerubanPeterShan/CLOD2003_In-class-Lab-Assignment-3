a6058f7706b5ed78d2a96e53b0b0250d2d2d762b
devIYKE <iykemadu99@gmail.com>
2024-10-06 13:24:03 -0400

    To get all task and get task ID

--- get_all_task.go
+++ get_all_task.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func getAllTasks(w http.ResponseWriter, r *http.Request, tasks []Task) {
+	if len(tasks) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "No tasks found")
+		return
+	}
+	json.NewEncoder(w).Encode(tasks)
 
 }
--- get_task_id.go
+++ get_task_id.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func getTaskID(w http.ResponseWriter, r *http.Request, tasks *[]Task, id int) {
-
+	for _, task := range *tasks {
+		if task.ID == id {
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Task not found")
 }
