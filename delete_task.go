ead10b5e40838a82b6993a829aec9f5d51757868
PoudelAshish <ashishpoudel@loyalistcollege.com>
2024-10-09 08:32:16 -0400

    first commit

--- delete_task.go
+++ delete_task.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func deleteTask(w http.ResponseWriter, r *http.Request, tasks *[]Task, id int) {
-
+	for i, task := range *tasks {
+		if task.ID == id {
+			*tasks = append((*tasks)[:i], (*tasks)[i+1:]...)
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "Task with ID %d deleted", id)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Task with ID %d not found", id)
 }
